f7201a2385df75380771c4df5b92e994fe73e308
JurassicSPb[credential] <Jurassic5@yandex.ru>
2017-06-05 00:29:11 +0300

    * package name

---
+++ app/src/main/java/com/ggl/jr/cookbooksearchbyingredients/fragments/FragmentInterface.java
@@ -0,0 +1,9 @@
+package com.ggl.jr.cookbooksearchbyingredients.fragments;
+
+/**
+ * Created by Мария on 16.02.2017.
+ */
+
+public interface FragmentInterface {
+    void fragmentBecameVisible();
+}
